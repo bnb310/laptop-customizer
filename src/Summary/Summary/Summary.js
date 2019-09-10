c8a72dc1d003ea66a0b27a73a883cf44f36de777
Nicki Pitcher <50596030+bnb310@users.noreply.github.com>
2019-09-09 19:30:58 -0600

    Update Summary.js

--- src/Summary/Summary/Summary.js
+++ src/Summary/Summary/Summary.js
@@ -1,22 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-
-class Summary extends Component {
-    render () {
-        var summary = Object.keys(this.state.selected).map((feature, idx) => {
-            const featureHash = feature + '-' + idx;
-            const selectedOption = this.state.selected[feature];
-          });
-        return (
-            <div className="summary__option" key={featureHash}>
-              <div className="summary__option__label">{this.props.feature} </div>
-              <div className="summary__option__value">{selectedOption.name}</div>
-              <div className="summary__option__cost">
-                {this.props.USCurrencyFormat.format(selectedOption.cost)}
-              </div>
-            </div>
-          );
-    }
-}
+const Summary = (props) => {
+    return (
+      <div className="summary__option" key={props.featureHash}>
+        <div className="summary__option__label">{props.feature} </div>
+        <div className="summary__option__value">{props.selectedOption.name}</div>
+        <div className="summary__option__cost">
+          {(props.USCurrencyFormat).format(props.selectedOption.cost)}
+        </div>
+      </div>
+    );
+  };
 
 export default Summary;
